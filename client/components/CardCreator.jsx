77a45d475366a49f343320aa6be56d70026ccd0f
Annie <annie.difiore@gmail.com>
2018-03-30 18:17:27 -0700

    refactor

--- client/components/CardCreator.jsx
+++ client/components/CardCreator.jsx
@@ -5,14 +5,14 @@ import RaisedButton from 'material-ui/RaisedButton';
 
 import {orange500, amber500, cyan500} from 'material-ui/styles/colors';
 
-// Right now, this component has props.recipeList and props.addCard
-const RecipeCreator = props => (
+// Right now, this component has props.CardList and props.addCard
+const CardCreator = props => (
   <Paper style={{backgroundColor: orange500, height:500, textAlign: 'center',  width:'50%', display:'inline-block'}}>
-    <p id="creatorTitle" style={{fontSize:40, paddingTop:40}}>Add New Recipe</p>
+    <p id="creatorTitle" style={{fontSize:40, paddingTop:40}}>Add New Card</p>
 
     <TextField
       id="nameInput"
-      hintText="Recipe Name"
+      hintText="Card Name"
       style={{width:'80%'}}
       underlineStyle={{borderColor: cyan500}}
       underlineFocusStyle={{borderColor: amber500}} />
@@ -51,12 +51,12 @@ const RecipeCreator = props => (
   </Paper>
 );
 
-export default RecipeCreator;
+export default CardCreator;
 
 
 // This didn't work with react, because the "action" property automatically redirected to root
 {/* <form method="POST" action="/">
-  <label>Name: <input type="text" name="recipeName" onChange={props.handleChange} /></label>
+  <label>Name: <input type="text" name="CardName" onChange={props.handleChange} /></label>
   <label>Category: <input type="text" name="category" onChange={props.handleChange} /></label>
   <label>URL: <input type="text" name="url" onChange={props.handleChange} /></label>
   <label>Notes: <input type="text" name="notes" onChange={props.handleChange} /></label>
